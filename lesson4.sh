5839368b844582df2f57b90079fdfc4dd6c652c4
Jienshakh <jienshakh@mail.ru>
2025-11-03 14:12:57 +0000

    lesson4

--- lesson4.sh
+++ lesson4.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd /opt
-git clone 
-
+sudo mkdir $USER
+sudo chown -R $USER $USER
+cd $USER
+git clone git@github.com:Jienshakh/shvirtd-example-python.git
+cd shvirtd-example-python
+docker compose up -d
+docker ps
 
